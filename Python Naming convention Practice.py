a3f2d322c70bc675a5f1a34aed2850d4664db0d8
JohnLV07 <edwinjlv07@gmail.com>
2024-08-11 18:00:43 -0500

    upadting

--- Python Naming convention Practice.py
+++ Python Naming convention Practice.py
@@ -1,7 +1,7 @@
 #Objective: The aim of this assignment is to understand and apply Python's naming conventions, particularly for variables and constants.
 #Task:Code correction:You have been given a piece of code with several variable and constant names that don't follow the Python naming convention. Your task is to correct them:
 
-pi_value = 3.14
+PI_VALUE = 3.14
 user_age = 25
 user_location = "New York"
-max_limit = 1000
+MAX_VALUE = 1000
